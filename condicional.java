6ae0be19b129a2f8de65476a78e3de36670adaf8
elizamunslinger1 <135707495+elizamunslinger1@users.noreply.github.com>
2024-10-03 08:40:58 -0300

    Add files via upload

---
+++ condicional.java
@@ -0,0 +1,17 @@
+public class condicional{}
+    public static void main(string[] args){
+        int anoDeLancamento = 2010; 
+        boolean incluidoNoPlano = true;
+        dooble notaDoFilme = 8.1;
+    string tipoPlano = "plus";
+
+    }ELSE{
+ System.out.println(" lançamento que os cliente curtiram!");
+}
+
+  if (incluidoNoPlano == true || tipoPlano.equals("plus")) {
+     system.out.println("Filme liberado");
+  }else{
+    system.out.println("Deve fazer locoção"){
+
+}
---
+++ filmes.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Este é o Screen Match");
+            system.out.println("Filme: Enrolados");
+
+int anoDeLancamento = 2010;
+ System.out.println( "Ano de lançando" + anoDeLancamento );
+ boolean incluidoNoPlano = true;
+ dooble notaDoFilme = 8.1;
+   //Média Calculada pelas notas de Ana, Joana e Suelem
+ dodble media= (9.8+ 10 + 6.0) /3;
+ system.out.println(media);
+ String sinopse;
+ sinopse = """
+             Filme top
+             Filme de animação com princesa da disney
+             Muito bom!
+             Ano de lançamento 
+             """ + anoDeLancamento;
+
+ Syestem.out.println(sinopse);
+ 
+   int classificação = (int) (media /2);
+      System.out.println(classificação);
+      
+    }
+}
+
+
+
+
+ 
